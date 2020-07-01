1c3c3be6ec3279da23abeb334e3e33645b97b7ad
VukasinVujic <vukasinvujic021@gmail.com>
2020-07-01 19:52:35 +0200

    Add react navigation HomeScreen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const HomeScreen = (props) => {
+  return (
+    <View>
+      <Text>Lalalalala</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
